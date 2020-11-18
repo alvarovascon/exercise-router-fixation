b1e7513101101cec1e47e4f9ab1a9d7ba976e9db
alvaro <alvarovascon2@gmail.com>
2020-11-18 17:09:47 -0300

    fixating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+import Users from './Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/users" render={() => <Users greetingMessage="Hello" />} />
+          <Route path="/" component={Home} />
+        </Switch>
+
+      </BrowserRouter>
     );
   }
 }
